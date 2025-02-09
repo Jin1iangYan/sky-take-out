1bf3e1c119a8f7a932f0c900895d9baa1e1708ce
JinliangYan <yanmiku0206@outlook.com>
2025-02-10 03:12:49 +0800

    refactor: 移除generateDateListAndValueList函数，这个函数让代码更难阅读了

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -18,7 +18,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,64 +31,6 @@ public class ReportServiceImpl implements ReportService {
     private OrderDetailMapper orderDetailMapper;
 
     /**
-     * 生成日期和对应数据的逗号分隔字符串
-     *
-     * @param allDates 日期列表
-     * @param dataList 数据列表
-     * @param <T> 数据类型
-     * @param <V> 数据值类型
-     * @param dateExtractor 提取日期的函数
-     * @param valueExtractor 提取值的函数
-     * @return 包含日期和对应值的VO对象
-     */
-    @SuppressWarnings("unchecked")
-    private <T, V> String generateDateListAndValueList(
-            List<LocalDate> allDates,
-            List<T> dataList,
-            java.util.function.Function<T, LocalDate> dateExtractor,
-            java.util.function.Function<T, V> valueExtractor) {
-
-        // 将数据按日期存储到一个 Map 中，方便查找
-        Map<LocalDate, V> dataMap = dataList.stream()
-                .collect(Collectors.toMap(dateExtractor, valueExtractor));
-
-        // 初始化日期和对应值的字符串构建器
-        StringBuilder dateListStr = new StringBuilder();
-        StringBuilder valueListStr = new StringBuilder();
-
-        // 遍历日期范围
-        for (LocalDate date : allDates) {
-            // 日期列表
-            if (dateListStr.length() > 0) {
-                dateListStr.append(",");
-            }
-            dateListStr.append(date);
-
-            // 获取该日期的对应数据，如果没有数据则为默认值
-            V value = dataMap.get(date);
-
-            // 如果没有找到对应的值，则根据类型赋默认值
-            if (value == null) {
-                if (BigDecimal.class.isAssignableFrom(valueExtractor.apply(dataList.get(0)).getClass())) {
-                    value = (V) BigDecimal.ZERO; // 默认值为 BigDecimal.ZERO
-                } else if (Long.class.isAssignableFrom(valueExtractor.apply(dataList.get(0)).getClass())) {
-                    value = (V) Long.valueOf(0); // 默认值为 0L
-                } else if (Integer.class.isAssignableFrom(valueExtractor.apply(dataList.get(0)).getClass())) {
-                    value = (V) Integer.valueOf(0); // 默认值为 0
-                }
-            }
-
-            // 值列表
-            if (valueListStr.length() > 0) {
-                valueListStr.append(",");
-            }
-            valueListStr.append(value);
-        }
-
-        return dateListStr + "|" + valueListStr;
-    }
-
-    /**
      * 营业额统计
      *
      * @param begin 统计的开始时间
@@ -98,20 +39,33 @@ public class ReportServiceImpl implements ReportService {
      */
     @Override
     public TurnoverReportVO getTurnoverReport(LocalDate begin, LocalDate end) {
-        // 生成日期列表
         List<LocalDate> allDates = begin.datesUntil(end.plusDays(1)).collect(Collectors.toList());
-
-        // 查询数据库获取营业额数据
         List<TurnoverReportDataDTO> turnoverDataList = orderMapper.selectTurnoverByDateRange(
                 begin.atStartOfDay(), end.plusDays(1).atStartOfDay());
 
-        // 生成日期和营业额的逗号分隔字符串
-        String[] result = generateDateListAndValueList(allDates, turnoverDataList,
-                TurnoverReportDataDTO::getOrderDate, TurnoverReportDataDTO::getAmountSum).split("\\|");
+        StringBuilder dateListStr = new StringBuilder();
+        StringBuilder turnoverListStr = new StringBuilder();
+
+        for (LocalDate date : allDates) {
+            if (dateListStr.length() > 0) {
+                dateListStr.append(",");
+                turnoverListStr.append(",");
+            }
+            dateListStr.append(date);
+
+            // 获取该日期的对应营业额数据
+            BigDecimal turnover = turnoverDataList.stream()
+                    .filter(data -> data.getOrderDate().equals(date))
+                    .map(TurnoverReportDataDTO::getAmountSum)
+                    .findFirst()
+                    .orElse(BigDecimal.ZERO);
+
+            turnoverListStr.append(turnover);
+        }
 
         return TurnoverReportVO.builder()
-                .dateList(result[0])
-                .turnoverList(result[1])
+                .dateList(dateListStr.toString())
+                .turnoverList(turnoverListStr.toString())
                 .build();
     }
 
@@ -124,23 +78,43 @@ public class ReportServiceImpl implements ReportService {
      */
     @Override
     public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
-        // 生成日期列表
         List<LocalDate> allDates = begin.datesUntil(end.plusDays(1)).collect(Collectors.toList());
-
-        // 查询数据库获取用户统计数据
         List<UserReportDataDTO> userDataList = userMapper.selectUserReportByDateRange(
                 begin.atStartOfDay(), end.plusDays(1).atStartOfDay());
 
-        // 生成日期和新增用户、总用户的逗号分隔字符串
-        String[] result = generateDateListAndValueList(allDates, userDataList,
-                UserReportDataDTO::getDate, UserReportDataDTO::getNewUserCount).split("\\|");
-        String[] totalUserResult = generateDateListAndValueList(allDates, userDataList,
-                UserReportDataDTO::getDate, UserReportDataDTO::getTotalUserCount).split("\\|");
+        StringBuilder dateListStr = new StringBuilder();
+        StringBuilder newUserListStr = new StringBuilder();
+        StringBuilder totalUserListStr = new StringBuilder();
+
+        for (LocalDate date : allDates) {
+            if (dateListStr.length() > 0) {
+                dateListStr.append(",");
+                newUserListStr.append(",");
+                totalUserListStr.append(",");
+            }
+            dateListStr.append(date);
+
+            // 获取对应的新增用户数和总用户数
+            Long newUserCount = userDataList.stream()
+                    .filter(data -> data.getDate().equals(date))
+                    .map(UserReportDataDTO::getNewUserCount)
+                    .findFirst()
+                    .orElse(0L);
+
+            Long totalUserCount = userDataList.stream()
+                    .filter(data -> data.getDate().equals(date))
+                    .map(UserReportDataDTO::getTotalUserCount)
+                    .findFirst()
+                    .orElse(0L);
+
+            newUserListStr.append(newUserCount);
+            totalUserListStr.append(totalUserCount);
+        }
 
         return UserReportVO.builder()
-                .dateList(result[0])
-                .newUserList(result[1])
-                .totalUserList(totalUserResult[1])
+                .dateList(dateListStr.toString())
+                .newUserList(newUserListStr.toString())
+                .totalUserList(totalUserListStr.toString())
                 .build();
     }
 
@@ -153,32 +127,47 @@ public class ReportServiceImpl implements ReportService {
      */
     @Override
     public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
-        // 生成日期列表
         List<LocalDate> allDates = begin.datesUntil(end.plusDays(1)).collect(Collectors.toList());
-
-        // 查询数据库获取用户统计数据
         List<OrderReportDataDTO> orderReportDateList = orderMapper.selectOrderReportByDateRange(
                 begin.atStartOfDay(), end.plusDays(1).atStartOfDay());
 
-        String[] orderCountResult = generateDateListAndValueList(allDates, orderReportDateList,
-                OrderReportDataDTO::getDate, OrderReportDataDTO::getOrderCount).split("\\|");
-        String[] validOrderCountResult = generateDateListAndValueList(allDates, orderReportDateList,
-                OrderReportDataDTO::getDate, OrderReportDataDTO::getValidOrderCount).split("\\|");
+        StringBuilder orderCountListStr = new StringBuilder();
+        StringBuilder validOrderCountListStr = new StringBuilder();
 
-        // 订单完成率
-        Double orderCompletionRate = orderReportDateList.get(0).getOrderCompletionRate();
-        // 订单总数
-        Integer orderTotalCount = orderReportDateList.get(0).getOrderTotalCount();
-        // 有效订单总数
-        Integer validOrderTotalCount = orderReportDateList.get(0).getValidOrderTotalCount();
+        Double orderCompletionRate = 0.0;
+        Integer orderTotalCount = 0;
+        Integer validOrderTotalCount = 0;
+
+        for (LocalDate date : allDates) {
+            if (orderCountListStr.length() > 0) {
+                orderCountListStr.append(",");
+                validOrderCountListStr.append(",");
+            }
+            orderCountListStr.append(0); // 默认订单数为 0
+            validOrderCountListStr.append(0); // 默认有效订单数为 0
+
+            // 获取订单统计数据
+            OrderReportDataDTO data = orderReportDateList.stream()
+                    .filter(order -> order.getDate().equals(date))
+                    .findFirst()
+                    .orElse(null);
+
+            if (data != null) {
+                orderCompletionRate = data.getOrderCompletionRate();
+                orderTotalCount = data.getOrderTotalCount();
+                validOrderTotalCount = data.getValidOrderTotalCount();
+                orderCountListStr.append(data.getOrderCount());
+                validOrderCountListStr.append(data.getValidOrderCount());
+            }
+        }
 
         return OrderReportVO.builder()
-                .dateList(orderCountResult[0])
+                .dateList(String.join(",", allDates.stream().map(LocalDate::toString).collect(Collectors.toList())))
                 .orderCompletionRate(orderCompletionRate)
                 .totalOrderCount(orderTotalCount)
-                .orderCountList(orderCountResult[1])
+                .orderCountList(orderCountListStr.toString())
                 .validOrderCount(validOrderTotalCount)
-                .validOrderCountList(validOrderCountResult[1])
+                .validOrderCountList(validOrderCountListStr.toString())
                 .build();
     }
 
@@ -187,33 +176,25 @@ public class ReportServiceImpl implements ReportService {
      *
      * @param begin 统计的开始时间
      * @param end   统计的结束时间
-     * @return OrderReportVO
+     * @return SalesTop10ReportVO
      */
     @Override
     public SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end) {
-        // 生成日期列表
-        List<LocalDate> allDates = begin.datesUntil(end.plusDays(1)).collect(Collectors.toList());
-
-        // 查询数据库获取销售Top10数据
         List<SalesTop10ReportDataDTO> salesTop10ReportDateList = orderDetailMapper.selectSalesTop10ReportByDateRange(
                 begin.atStartOfDay(), end.plusDays(1).atStartOfDay());
 
-        // 创建两个字符串列表用于存储名字和销量
         StringBuilder nameListStr = new StringBuilder();
         StringBuilder numberListStr = new StringBuilder();
 
-        // 遍历销售Top10数据
         for (SalesTop10ReportDataDTO data : salesTop10ReportDateList) {
             if (nameListStr.length() > 0) {
                 nameListStr.append(",");
                 numberListStr.append(",");
             }
-            // 拼接名字和销量
             nameListStr.append(data.getName());
             numberListStr.append(data.getSaleNumber());
         }
 
-        // 返回构建的 VO 对象
         return SalesTop10ReportVO.builder()
                 .nameList(nameListStr.toString())
                 .numberList(numberListStr.toString())
